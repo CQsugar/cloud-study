a887ff6a866d0d747e463c206ade3d4730417b76
songchunqi <1186765234@qq.com>
2020-06-08 18:13:29 +0800

    eureke client(provide) 修改

--- cloud-provide-payment-8001/src/main/java/xyz/cchili/springcloud/CloudProvidePayment8001Application.java
+++ cloud-provide-payment-8001/src/main/java/xyz/cchili/springcloud/CloudProvidePayment8001Application.java
@@ -2,10 +2,12 @@ package xyz.cchili.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("xyz.cchili.springcloud.mapper")
+@EnableEurekaClient
 public class CloudProvidePayment8001Application {
 
     public static void main(String[] args) {
