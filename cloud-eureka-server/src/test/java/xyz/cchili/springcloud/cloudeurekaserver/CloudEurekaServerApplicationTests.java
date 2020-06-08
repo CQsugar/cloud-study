ce59f80606b252444032c8a681b5d4cc73f1a114
songchunqi <1186765234@qq.com>
2020-06-08 17:55:19 +0800

    eureka服务初始化

---
+++ cloud-eureka-server/src/test/java/xyz/cchili/springcloud/cloudeurekaserver/CloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.cchili.springcloud.cloudeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
