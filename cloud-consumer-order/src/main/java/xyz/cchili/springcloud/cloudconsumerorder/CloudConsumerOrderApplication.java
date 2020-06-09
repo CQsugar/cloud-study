13c9cedfcea3e25fc4979d06480d9de5e1066293
songchunqi <1186765234@qq.com>
2020-06-09 16:11:29 +0800

    DiscoveryClient添加

--- cloud-consumer-order/src/main/java/xyz/cchili/springcloud/cloudconsumerorder/CloudConsumerOrderApplication.java
+++ cloud-consumer-order/src/main/java/xyz/cchili/springcloud/cloudconsumerorder/CloudConsumerOrderApplication.java
@@ -2,10 +2,12 @@ package xyz.cchili.springcloud.cloudconsumerorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class CloudConsumerOrderApplication {
 
     public static void main(String[] args) {
--- cloud-consumer-order/src/main/java/xyz/cchili/springcloud/cloudconsumerorder/controller/OrderController.java
+++ cloud-consumer-order/src/main/java/xyz/cchili/springcloud/cloudconsumerorder/controller/OrderController.java
@@ -1,5 +1,8 @@
 package xyz.cchili.springcloud.cloudconsumerorder.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import xyz.cchili.springcloud.cloudapicommons.pojo.Payment;
 import xyz.cchili.springcloud.cloudapicommons.vo.Result;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author CQSuagr
@@ -16,6 +20,7 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping("/consumer/payment")
+@Slf4j
 public class OrderController {
 
     //private static final String URL = "http://localhost:8001";
@@ -24,6 +29,9 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/create")
     public Result create(Payment payment) {
@@ -38,5 +46,20 @@ public class OrderController {
     @GetMapping("/port")
     public Result port() {
         return restTemplate.getForObject(URL + "/payment/port", Result.class);
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("==={}===", service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+
+        return discoveryClient;
     }
 }
