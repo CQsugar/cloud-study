6c06ea896fced8a79e71798021c76e3063db7f01
songchunqi <1186765234@qq.com>
2020-06-10 15:27:51 +0800

    consul

--- cloud-provide-payment-8001/src/main/java/xyz/cchili/springcloud/CloudProvidePayment8001Application.java
+++ cloud-provide-payment-8001/src/main/java/xyz/cchili/springcloud/CloudProvidePayment8001Application.java
@@ -2,10 +2,12 @@ package xyz.cchili.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("xyz.cchili.springcloud.mapper")
+@EnableDiscoveryClient //该注解用于向使用者consul zookeeper作为注册中心时注册服务
 public class CloudProvidePayment8001Application {
 
     public static void main(String[] args) {
