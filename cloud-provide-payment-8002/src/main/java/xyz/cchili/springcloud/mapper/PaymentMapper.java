fa28ab2bfab722a1d26cf72727541250728400c4
songchunqi <1186765234@qq.com>
2020-06-20 00:57:30 +0800

    简化

--- cloud-provide-payment-8002/src/main/java/xyz/cchili/springcloud/mapper/PaymentMapper.java
+++
@@ -1,7 +0,0 @@
-package xyz.cchili.springcloud.mapper;
-
-import tk.mybatis.MyMapper;
-import xyz.cchili.springcloud.cloudapicommons.pojo.Payment;
-
-public interface PaymentMapper extends MyMapper<Payment> {
-}
